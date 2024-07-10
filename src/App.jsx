4f82151833c9861ca16809195789f037a6485aab
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-07-10 15:15:07 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export function App() {
+    return (
+        <h1 className="text-3xl font-bold text-gray-700">
+            Hello world!
+        </h1>
+    )
+}
+
+export default App
