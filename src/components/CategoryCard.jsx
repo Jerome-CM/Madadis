85fb7c0c2aed8469800e4131e34f19b2883caee4
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-07-19 23:01:02 +0200

    Change href and CSS

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,8 +1,8 @@
 export function CategoryCard ({name}) {
     return <>
-        <div className="w-5/12 rounded overflow-hidden shadow-lg p-4 m-5 bg-white text-black flex justify-between">
+        <div className="w-5/12 rounded overflow-hidden shadow-lg p-4 m-5 bg-white text-black flex justify-between items-center">
             <h2 className="ml-4">{name}</h2>
-            <a className="bg-slate-600 py-1 px-3 text-white rounded drop-shadow-md mr-4" href={"https://dummyjson.com/products/category/"+name}>
+            <a className="bg-slate-600 py-1 px-3 text-white rounded drop-shadow-md mr-4" href={"/categoryContent?category="+name}>
                 Show the products </a>
         </div>
     </>
