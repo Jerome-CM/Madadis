60e079baed4ec33e78e49333354963ec59f089c8
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-07-19 10:53:36 +0200

    Change title

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,7 @@ import CardProduct from "../components/CardProduct.jsx";
 
 export function Products() {
     return <>
+        <h1 className="w10/12 text-center text-black text-2xl p-2 my-5">Products</h1>
         <GetProducts numberOfProduct={25} select={'title,thumbnail'}>
             {(products) => (
                 <div className="flex flex-wrap justify-center">
