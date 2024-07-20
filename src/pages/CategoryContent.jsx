b1675d1e0cf418f7e2494d96f9f4688dea721f4d
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-07-20 23:01:31 +0200

    remove bracket on import

--- src/pages/CategoryContent.jsx
+++ src/pages/CategoryContent.jsx
@@ -1,5 +1,23 @@
+import CardProduct from "../components/CardProduct.jsx";
+import {GetGategoryContent} from "../components/get/GetCategoryContent";
+import GetValueOfParamInUrl from "../script/GetValueOfParamInUrl.jsx";
+
 export function CategoryContent() {
+
+    const categoryName = GetValueOfParamInUrl('category');
+
     return <>
-        <h1>content category</h1>
+        <h1 className="w10/12 text-center text-black text-2xl p-2 my-5">Content of the {categoryName} category</h1>
+        <GetGategoryContent>
+            {/*Faire un composant title vu qu'il y a le meme style*/}
+            {(products) => (
+                <div className="flex flex-wrap justify-center">
+                    {products.map(product => (
+                        <CardProduct key={product.id} product={product} />
+                    ))}
+                </div>
+            )}
+        </GetGategoryContent>
+
     </>
 }
--- src/script/GetValueOfParamInUrl.jsx
+++ src/script/GetValueOfParamInUrl.jsx
@@ -1,5 +1,7 @@
-export function getValueOfParamInUrl(param){
+function getValueOfParamInUrl(param){
     const urlFind = new URL(window.location.href);
     const params = new URLSearchParams(urlFind.search);
     return params.get(param);
 }
+
+export default getValueOfParamInUrl;
