6150f39a855275178bc02df51f7714cb17bf14ff
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-07-21 16:07:08 +0200

    English and hover button colors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 export function NavBar() {
     return <nav className="flex flex-row justify-between p-3 bg-cyan-950 font-medium items-center">
             <span className="basis-1/4 mx-1 text-4xl">Madadis</span>
-            <ul className="basis-2/4 flex flex-row gap-6 justify-end mx-1 p-2">
-                <li>
+            <ul className="basis-2/4 flex flex-row gap-5 justify-end mx-1 p-2">
+                <li className="rounded hover:bg-cyan-600 px-4 py-2">
                     <Link to="/home">Home</Link>
                 </li>
-                <li>
-                    <Link to="/products">Produits</Link>
+                <li className="rounded hover:bg-cyan-600 px-4 py-2">
+                    <Link to="/products">Products</Link>
                 </li>
-                <li>
-                    <Link to="/categories">Catégories</Link>
+                <li className="rounded hover:bg-cyan-600 px-4 py-2">
+                    <Link to="/categories">Categories</Link>
                 </li>
-                <li>
-                    <Link to="/shopping">Panier</Link>
+                <li className="rounded hover:bg-cyan-600 px-4 py-2">
+                    <Link to="/shopping">Cart</Link>
                 </li>
-                <li>
+                <li className="rounded hover:bg-cyan-600 px-4 py-2">
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
