27e4e393d060814a3668463cc8c66a1477c9eb84
Jérôme <100235233+Jerome-CM@users.noreply.github.com>
2024-09-13 15:37:47 +0200

    Change with for img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export function Home() {
     return (
         <>
             <MainTitle title={'Welcome'}/>
-            <img className="d-flex mx-auto my-6" src="src/assets/react.png" alt="React logo" />
+            <img className="d-flex mx-auto my-6 w-3/12" src="src/assets/react.png" alt="React logo" />
             <GetProducts numberOfProduct={6} select={'title,thumbnail'}>
                 {(products) => (
                     <div className="flex flex-wrap justify-center">
